b5a428450234d107819bdde0b93476d0d89604d2
Kyle Walker <kylewalker@Caths-MacBook-Air.local>
2019-05-20 15:31:09 +0100

    fixed issue when login attempt with non-existent username would not error

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -3,10 +3,9 @@ const { getUser } = require('../models/usersModels');
 exports.sendUser = (req, res, next) => {
   getUser(req.params)
     .then(([user]) => {
-      // if (user === undefined)
-      //   return Promise.reject({ code: 404, msg: 'User Not Found' });
-      // else
-      res.status(200).send({ user });
+      if (user === undefined)
+        return Promise.reject({ code: 404, msg: 'User Not Found' });
+      else res.status(200).send({ user });
     })
     .catch(next);
 };
